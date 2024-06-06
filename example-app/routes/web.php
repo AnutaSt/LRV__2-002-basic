3fae92765d8055077b174d6f8a6db60aae7ab768
AnnaSt <anutabatuk@gmail.com>
2024-06-06 19:48:26 +0800

    check_check

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/groups');
 });
 
 Route::get('/groups', [\App\Http\Controllers\GroupController::class, 'index']);
